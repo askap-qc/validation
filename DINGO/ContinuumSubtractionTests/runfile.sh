bbdea3e2b74eb7e618a36cf02e994fa849b19a6b
sambitrc <62928892+sambitrc@users.noreply.github.com>
2020-08-28 11:17:38 +0800

    Update runfile.sh

--- DINGO/ContinuumSubtractionTests/runfile.sh
+++ DINGO/ContinuumSubtractionTests/runfile.sh
@@ -1,4 +1,4 @@
-awk '!/#/' selavy-results_<SB number>_<tile number_<beam number>.txt | awk '{print $8,$9,$19}' >  selavy-results_<SB number>_<tile number_beam<number>_gist.txt
+awk '!/#/' selavy-results.txt | awk '{print $8,$9,$19}' >  selavy-results_<SB number>_<tile number_<beam number>_gist.txt
 python extractpixspecs.py <SB number> <tile number> <beam number>
 python plotfinal.py <SB number> <tile number> <beam number>
 pdfjoin finalplots/*<SB number>_<tile number>_<beam number>*png -o finalplots/<SB number>_<tile number>_<beam number>_contsub.pdf
