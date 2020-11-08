444542f216e42b5ec5409bf782d09d5185becad3
Matthew Whiting <matthew.whiting@csiro.au>
2020-11-08 21:42:23 +1100

    Allowing the base part of the image name to be centrally defined, to make changing it easy.

--- wallaby_hi_val.py
+++ wallaby_hi_val.py
@@ -421,7 +421,7 @@ def qc_BeamLogs():
     """
     
     file_dir = 'SpectralCube_BeamLogs'
-    basename = '/beamlog.image.restored.i.SB'+ sbid + '.cube.'+ field
+    basename = '/beamlog.image.restored.' + imagebase + field
     QC_BEAMS_LABEL = []
     
     for i in range(0,36):
@@ -541,7 +541,7 @@ def FlagStat_plot(FLAGSTAT, n):
     """
 
     file_dir = diagnostics_dir +'/cubestats-'+ field 
-    basename = '/cubeStats-image.restored.i.SB'+ sbid +'.cube.'+ field  
+    basename = '/cubeStats-image.restored.' + imagebase + field  
     
     title = 'Flagged Fraction'
     plot_name = 'FlagStat.png'
@@ -719,7 +719,7 @@ def NoiseRank_histplot(nchan):
     plot_name = 'beam_1pctile_hist_SB'+ sbid + '.png'
     saved_fig = fig_dir + '/' + plot_name
     file_dir = diagnostics_dir +'/cubestats-'+ field 
-    basename = '/cubeStats-image.restored.i.SB'+ sbid +'.cube.'+ field
+    basename = '/cubeStats-image.restored.' + imagebase + field
 
     params = {'axes.labelsize': 6,
               'axes.titlesize':6,
@@ -875,7 +875,7 @@ def BeamStat_plot(item, n):
     Plotting and visualising statistics of 36 beams. 
     """
     file_dir = diagnostics_dir +'/cubestats-'+ field 
-    basename = '/cubeStats-image.restored.i.SB'+ sbid +'.cube.'+ field  
+    basename = '/cubeStats-image.restored.' + imagebase + field  
 
     params = {'axes.labelsize': 10,
               'axes.titlesize':10,
@@ -936,6 +936,8 @@ n = [26,25,24,23,22,21,27,10,9,8,7,20,28,11,3,1,6,19,29,12,2,0,5,18,30,13,14,15,
 html_name = 'index.html'
 diagnostics_dir = 'diagnostics'
 
+imagebase='i.SB'+sbid+'.cube.'
+
 if not os.path.isdir(fig_dir):
     os.system('mkdir '+ fig_dir)
 
@@ -945,7 +947,7 @@ metafile = sorted(glob.glob('metadata/mslist-*txt'))[0]
 metafile_science = sorted(glob.glob('metadata/mslist-scienceData*txt'))[0]
 metafile_cal = sorted(glob.glob('metadata/mslist-cal*txt'))[0]
 param_file = sorted(glob.glob('slurmOutput/*.sh'))
-fitsimage = ('image.restored.i.SB' + sbid + '.cube.contsub.fits')
+fitsimage = ('image.restored.'+ imagebase + 'contsub.fits')
 
 # Check if there is more than one parameter input .sh file in the slurmOutput directory.
 # If it does, select the latest one.
