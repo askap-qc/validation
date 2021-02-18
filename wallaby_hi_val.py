d9c4c9248fab7066f94c3df0818fc61d6aa76f83
Matthew Whiting <matthew.whiting@csiro.au>
2021-02-18 15:47:02 +1100

    Changes to the get_HIPASS function: * increased timeout (just to cover occasional badness on Pawsey compute nodes) * checks for the instances of no HIPASS sources being returned

--- wallaby_hi_val.py
+++ wallaby_hi_val.py
@@ -114,9 +114,11 @@ def get_HIPASS(ra, dec):
     """
 
     print ("Retrieving HIPASS sources from Vizier. Depending on server connection, this might take a while......")
+
+    catalogue='VIII/73/hicat'
     
     Vizier.ROW_LIMIT = -1
-    v = Vizier(columns=['HIPASS', '_RAJ2000', '_DEJ2000', 'RVsp', 'Speak', 'Sint', 'RMS', 'Qual'], catalog = 'VIII/73/hicat', timeout=1000)
+    v = Vizier(columns=['HIPASS', '_RAJ2000', '_DEJ2000', 'RVsp', 'Speak', 'Sint', 'RMS', 'Qual'], catalog = catalogue, timeout=10000)
 
     TOKS_RA = ra.split(":")
     ra_hr = float(TOKS_RA[0])
@@ -132,7 +134,10 @@ def get_HIPASS(ra, dec):
     hipass_result = v.query_region(coord.SkyCoord(ra=ra_deg, dec=dec_tdeg, unit=(u.deg, u.deg), frame='icrs'), width=[6*u.deg])
 
     hipass_cat = 'hipass.txt'
-    print (hipass_result['VIII/73/hicat'], file=open(fig_dir + '/' + hipass_cat,'w'))
+    if hipass_result.keys()==[catalogue]:
+        print (hipass_result[catalogue], file=open(fig_dir + '/' + hipass_cat,'w'))
+    else:
+        print ('No HIPASS sources within 6 degrees', file=open(fig_dir + '/' + hipass_cat,'w'))
 
     return hipass_cat
 
