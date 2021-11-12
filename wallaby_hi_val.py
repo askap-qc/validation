5c3ebd12f64152db49927bf8c451d0f55f0afe63
Matthew Whiting <matthew.whiting@csiro.au>
2021-11-12 11:12:11 +1100

    Adding a couple of additional checks for the existence of files, to avoid trying to plot things when we don't have the info

--- wallaby_hi_val.py
+++ wallaby_hi_val.py
@@ -971,8 +971,8 @@ def BeamStat_plot(item, n):
             if item == 'Avg_RMS':
                 beamstat = cal_beam_AvgRMS(infile)
 
-        plt.scatter([beamXPOS[i]], [beamYPOS[i]], s=1300, c=[beamstat], cmap='RdYlGn_r', edgecolors='black', vmin=vmin, vmax=vmax)
-        plt.text(beamXPOS[i], beamYPOS[i], n[i], va='center', ha='center')
+            plt.scatter([beamXPOS[i]], [beamYPOS[i]], s=1300, c=[beamstat], cmap='RdYlGn_r', edgecolors='black', vmin=vmin, vmax=vmax)
+            plt.text(beamXPOS[i], beamYPOS[i], n[i], va='center', ha='center')
 
     plt.xlim(0,0.7)
     plt.ylim(0,1.4)
@@ -1032,24 +1032,25 @@ def ResBeam_Stats_plot(n, header_bmaj, header_bmin):
     for i in range(0,36):
         bnum = n[i]
         infile = file_dir + basename +'.beam%02d.txt'%(bnum)
-        bmaj_stdev, bmin_stdev, beam_threshold, max_ratio_BA, min_ratio_BA = cal_ResBeam_Stats(infile, header_bmaj, header_bmin)
-        BEAM_THRESHOLD.append(beam_threshold)
+        if os.path.isfile(infile):
+            bmaj_stdev, bmin_stdev, beam_threshold, max_ratio_BA, min_ratio_BA = cal_ResBeam_Stats(infile, header_bmaj, header_bmin)
+            BEAM_THRESHOLD.append(beam_threshold)
 
-        ax1.scatter([beamXPOS[i]], [beamYPOS[i]], s=1400, edgecolors='black', facecolors='none')
-        ax1.text(beamXPOS[i], beamYPOS[i]+0.02, n[i], va='center', ha='center')
-        ax1.text(beamXPOS[i], beamYPOS[i]-0.02, round(bmaj_stdev, 3), va='center', ha='center', fontsize=8, color='blue')
+            ax1.scatter([beamXPOS[i]], [beamYPOS[i]], s=1400, edgecolors='black', facecolors='none')
+            ax1.text(beamXPOS[i], beamYPOS[i]+0.02, n[i], va='center', ha='center')
+            ax1.text(beamXPOS[i], beamYPOS[i]-0.02, round(bmaj_stdev, 3), va='center', ha='center', fontsize=8, color='blue')
 
-        ax2.scatter([beamXPOS[i]], [beamYPOS[i]], s=1400, edgecolors='black', facecolors='none')
-        ax2.text(beamXPOS[i], beamYPOS[i]+0.02, n[i], va='center', ha='center')
-        ax2.text(beamXPOS[i], beamYPOS[i]-0.02, round(bmin_stdev,3), va='center', ha='center', fontsize=8, color='blue')
+            ax2.scatter([beamXPOS[i]], [beamYPOS[i]], s=1400, edgecolors='black', facecolors='none')
+            ax2.text(beamXPOS[i], beamYPOS[i]+0.02, n[i], va='center', ha='center')
+            ax2.text(beamXPOS[i], beamYPOS[i]-0.02, round(bmin_stdev,3), va='center', ha='center', fontsize=8, color='blue')
 
-        maxplot = ax3.scatter([beamXPOS[i]], [beamYPOS[i]], s=1300, c=[max_ratio_BA], cmap='summer', edgecolors='black', vmin=0, vmax=1.1)
-        ax3.text(beamXPOS[i], beamYPOS[i]+0.02, n[i], va='center', ha='center')
-        ax3.text(beamXPOS[i], beamYPOS[i]-0.02, round(max_ratio_BA,3), va='center', ha='center', fontsize=8, color='blue')
+            maxplot = ax3.scatter([beamXPOS[i]], [beamYPOS[i]], s=1300, c=[max_ratio_BA], cmap='summer', edgecolors='black', vmin=0, vmax=1.1)
+            ax3.text(beamXPOS[i], beamYPOS[i]+0.02, n[i], va='center', ha='center')
+            ax3.text(beamXPOS[i], beamYPOS[i]-0.02, round(max_ratio_BA,3), va='center', ha='center', fontsize=8, color='blue')
         
-        minplot = ax4.scatter([beamXPOS[i]], [beamYPOS[i]], s=1300, c=[min_ratio_BA], cmap='summer', edgecolors='black', vmin=0, vmax=1.1)
-        ax4.text(beamXPOS[i], beamYPOS[i]+0.02, n[i], va='center', ha='center')
-        ax4.text(beamXPOS[i], beamYPOS[i]-0.02, round(min_ratio_BA,3), va='center', ha='center', fontsize=8, color='blue')
+            minplot = ax4.scatter([beamXPOS[i]], [beamYPOS[i]], s=1300, c=[min_ratio_BA], cmap='summer', edgecolors='black', vmin=0, vmax=1.1)
+            ax4.text(beamXPOS[i], beamYPOS[i]+0.02, n[i], va='center', ha='center')
+            ax4.text(beamXPOS[i], beamYPOS[i]-0.02, round(min_ratio_BA,3), va='center', ha='center', fontsize=8, color='blue')
         
     ax1.set_xlim(0,0.7)
     ax1.set_ylim(0,1.4)
